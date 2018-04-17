02690140082e7d7bafbf9444df3fabe4d66c6904
Eloisa <eloisa11victoria@gmail.com>
2018-04-17 18:42:18 -0300

    Colocando as alterações do master no meu branch funcao-inserir

--- src/br/lisianthus/utils/ManipulaString.java
+++
@@ -1,8 +0,0 @@
-package br.lisianthus.utils;
-
-public class ManipulaString {
-	public static String capitularizar(String str){
-		if(str==null) return null;
-		return str.substring(0,1).toUpperCase()+str.substring(1);
-	}
-}
